bfd44730b9a80b86104856d4ea55c67db1e482b9
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-04-15 12:58:48 +0300

    Add contact page content

--- src/contact.js
+++ src/contact.js
@@ -11,6 +11,36 @@ function addContactInformation() {
     const contactTitle = document.createElement('h1');
     contactTitle.textContent = "Contact us";
     info.appendChild(contactTitle);
+
+    function createContact(contactItemText, contactItemPhoneText) {        
+        const contactContainer = document.createElement('div');
+        contactContainer.classList.add('contact-container');
+        if (contactItemText === "Customer service") {
+            contactContainer.classList.add("first-contact-container");
+        }
+
+        for (let i = 0; i < 3; i++) {
+            let contactItem = document.createElement('p');
+            contactItem.classList.add('contact-item');
+            switch(i) {
+                case 0:
+                    contactItem.textContent = contactItemText;
+                    break;
+                case 1:
+                    contactItem.textContent = contactItemPhoneText;
+                    break;
+                case 2:
+                    contactItem.textContent = contactItemText.split(" ")[0].toLowerCase() + "MK" + "example@com";
+                    break;
+            }
+            contactContainer.appendChild(contactItem);
+        }
+        info.appendChild(contactContainer);
+    }
+
+    createContact("Customer service", "1-234-567-20-07");
+    createContact("Development department", "1-234-567-20-08");
+    createContact("Business & cooperation", "1-234-567-20-09");
 }
 
 export {addContactInformation};
--- src/homepage.js
+++ src/homepage.js
@@ -1,70 +1,69 @@
 function createHomepage(){
     
-const info = document.createElement('div');
-const content = document.querySelector('.content');
+    const info = document.createElement('div');
+    const content = document.querySelector('.content');
 
-content.style.display = "flex";
-content.style.padding = 0;
+    content.style.display = "flex";
+    content.style.padding = 0;
 
-info.classList.add('info');
-content.appendChild(info);
+    info.classList.add('info');
+    content.appendChild(info);
 
-const title = document.createElement('h1');
-title.textContent = "Chef Mc Kenzy's restaurant";
-info.appendChild(title);
+    const title = document.createElement('h1');
+    title.textContent = "Chef Mc Kenzy's restaurant";
+    info.appendChild(title);
 
-const quote = document.createElement('q');
-const quoteText = document.createElement('em');
-quoteText.textContent = "One cannot think well, love well, sleep well, if one has not dined well.";
-quote.appendChild(quoteText);
-info.appendChild(quote);
+    const quote = document.createElement('q');
+    const quoteText = document.createElement('em');
+    quoteText.textContent = "One cannot think well, love well, sleep well, if one has not dined well.";
+    quote.appendChild(quoteText);
+    info.appendChild(quote);
 
-const quoteAuthor = document.createElement('p');
-quoteAuthor.textContent = "- Virginia Woolf";
-info.appendChild(quoteAuthor);
+    const quoteAuthor = document.createElement('p');
+    quoteAuthor.textContent = "- Virginia Woolf";
+    info.appendChild(quoteAuthor);
 
-const workingHours = document.createElement('h2');
-workingHours.textContent = "Working hours";
-info.appendChild(workingHours);
+    const workingHours = document.createElement('h2');
+    workingHours.textContent = "Working hours";
+    info.appendChild(workingHours);
 
-const hourList = document.createElement('ul');
-for (let i = 0; i < 7; i++) {
-    let item = document.createElement('li');
-    switch(i) {
-        case 0:
-            item.textContent = "Sunday: 10am - 8pm";
-            break;
-        case 1:
-            item.textContent = "Monday: 9am - 9pm";
-            break;
-        case 2:
-            item.textContent = "Tuesday: 9am - 9pm";
-            break;
-        case 3:
-            item.textContent = "Wednesday: 9am - 9pm";
-            break;
-        case 4:
-            item.textContent = "Thursday: 9am - 9pm";
-            break;
-        case 5:
-            item.textContent = "Friday: 9am - 9pm";
-            break;       
-        case 6:
-            item.textContent = "Saturday: 10am - 8pm";     
+    const hourList = document.createElement('ul');
+    for (let i = 0; i < 7; i++) {
+        let item = document.createElement('li');
+        switch(i) {
+            case 0:
+                item.textContent = "Sunday: 10am - 8pm";
+                break;
+            case 1:
+                item.textContent = "Monday: 9am - 9pm";
+                break;
+            case 2:
+                item.textContent = "Tuesday: 9am - 9pm";
+                break;
+            case 3:
+                item.textContent = "Wednesday: 9am - 9pm";
+                break;
+            case 4:
+                item.textContent = "Thursday: 9am - 9pm";
+                break;
+            case 5:
+                item.textContent = "Friday: 9am - 9pm";
+                break;       
+            case 6:
+                item.textContent = "Saturday: 10am - 8pm";     
+        }
+        hourList.appendChild(item);
     }
-    hourList.appendChild(item);
-}
 
-info.appendChild(hourList);
+    info.appendChild(hourList);
 
-const locationTitle = document.createElement('h2');
-locationTitle.textContent = "Location";
-info.appendChild(locationTitle);
+    const locationTitle = document.createElement('h2');
+    locationTitle.textContent = "Location";
+    info.appendChild(locationTitle);
 
-const location = document.createElement('p');
-location.textContent = "1533 Earnhardt Drive, Kentucky";
-info.appendChild(location);
-
+    const location = document.createElement('p');
+    location.textContent = "1533 Earnhardt Drive, Kentucky";
+    info.appendChild(location);
 }
 
 function resetContent() {
